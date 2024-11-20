8449c3a34a9b1a299d50ae472f4d41d69990a7ac
khaled Ahmed <akhaled795@gmail.com>
2024-11-20 15:50:41 +0300

    finished dashboard theme

---
+++ resources/views/dashboard/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard.app')
+@section('title')
+Welcome
+@endsection
+@section('content')
+<h1>Khaled Ahmed AbdelFatah Mohamed Ali Quora</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
+], function () {
+    Route::get('/test', function () {
+        return view('dashboard.welcome');
+    });
 });
+
